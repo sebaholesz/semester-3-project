ce1f59a02b01f95a8ab9e2cfd92d1fef06edf40f
sebaholesz <sebastianholesz@seznam.cz>
2020-10-31 19:08:46 +0100

    Finished tracer bullet
    I finished the tracer bullet so RN we are able to
    
    >Call the API from both WEB and WPF
    >Recieve the call in the API, send it "down" and get data from the DB
    >Transfer it back "up" and display it in the UI of both the WEB and WPF
    >Navigate within the WEB using the routeconfig

--- semester-3-project/WebApplication/App_Start/RouteConfig.cs
+++ semester-3-project/WebApplication/App_Start/RouteConfig.cs
@@ -13,9 +13,28 @@ namespace WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //manual routing is up for a debate... but it will prbably make it easier for use to set them up manually... 
+            routes.MapRoute(
+                name: "UserIndex",
+                url: "user/{action}/{id}",
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "UserMyAccount",
+                url: "user/my-account",
+                defaults: new { controller = "User", action = "MyAccount", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "contact",
+                defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
