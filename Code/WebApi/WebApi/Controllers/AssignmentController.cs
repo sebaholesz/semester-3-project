9db788377f92c3dc2540dcbd2451f40e66d5283c
Sam Horacek <sam.horacek@gmail.com>
2020-11-10 11:19:51 +0100

    General AssignmentController added, to be finished when Model & DAL are done. ../../BusinessLayer/obj/Debug/BusinessLayer.csprojAssemblyReference.cache ../../BusinessLayer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache ../../DatabaseLayer/obj/Debug/DatabaseLayer.csprojAssemblyReference.cache ../../DatabaseLayer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache ../../ModelLayer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache ../../WebApi.Tests/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache ../../../WebApplication/WebApplication.Tests/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csprojAssemblyReference.cache ../../BusinessLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll ../../BusinessLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml ../../DatabaseLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll ../../DatabaseLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml ../../ModelLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll ../../ModelLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml ../../packages/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.3.6.0/ ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.CopyComplete ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.CoreCompileInputs.cache ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.FileListAbsolute.txt ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.dll ../../../WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.pdb ../../../WebApplication/packages/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.3.6.0/

---
+++ Code/WebApi/WebApi/Controllers/AssignmentController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class AssignmentController : ApiController
+    {
+
+        //private assignmentInterface class
+
+        public AssignmentController()
+        {
+            //instantiate assignmentInterface class
+        }
+
+        //[Route("Assignment")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            //assignmentInterface getAll
+            //return in HttpResonseMessage body List<Assignment>
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            return httpResponseMessage;
+        }
+
+
+        //[Route("Assignment/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            //assignmentInterface get
+            //return in HttpResonseMessage body Assignment
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            return httpResponseMessage;
+        }
+
+
+        //[Route("Assignment")]
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            //assignmentInterface create
+            //return in HttpResonseMessage body Assignment
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            return httpResponseMessage;
+
+        }
+
+
+        //[Route("Assignment/{id}")]
+        [HttpPost]
+        public HttpResponseMessage Post(int id)
+        {
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            
+        }
+
+        [Route("Assignment")]
+        [HttpPut]
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+
+        [Route("Assignment/{id}")]
+        [HttpPut]
+        public HttpResponseMessage Put(int id)
+        {
+            //assignmentInterface update
+            //return in HttpResonseMessage body Assignment
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            return httpResponseMessage;
+        }
+
+        [Route("Assignment")]
+        [HttpDelete]
+        public HttpResponseMessage Delete()
+        {
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+
+        [Route("Assignment/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            //assignmentInterface delete
+            //return if operation was successful
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            return httpResponseMessage;
+        }
+
+
+    }
+}
