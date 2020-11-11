af2e357ba30bb42d8605c40ae920e75a567e91bf
Sam Horacek <sam.horacek@gmail.com>
2020-11-11 14:04:00 +0100

    Code/WebApi/BusinessLayer/obj/Debug/BusinessLayer.csproj.CoreCompileInputs.cache Code/WebApi/BusinessLayer/obj/Debug/BusinessLayer.csproj.FileListAbsolute.txt Code/WebApi/BusinessLayer/obj/Debug/BusinessLayer.csprojAssemblyReference.cache Code/WebApi/DatabaseLayer/obj/Debug/DatabaseLayer.csproj.CoreCompileInputs.cache Code/WebApi/DatabaseLayer/obj/Debug/DatabaseLayer.csproj.FileListAbsolute.txt Code/WebApi/DatabaseLayer/obj/Debug/DatabaseLayer.csprojAssemblyReference.cache Code/WebApi/DatabaseLayer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache Code/WebApi/ModelLayer/obj/Debug/ModelLayer.csprojAssemblyReference.cache Code/WebApi/UnitTestWebApiSolution/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache Code/WebApi/UnitTestWebApiSolution/obj/Debug/UnitTestWebApiSolution.csproj.CoreCompileInputs.cache Code/WebApi/UnitTestWebApiSolution/obj/Debug/UnitTestWebApiSolution.csproj.FileListAbsolute.txt Code/WebApi/UnitTestWebApiSolution/obj/Debug/UnitTestWebApiSolution.csprojAssemblyReference.cache Code/WebApi/WebApi.Tests/obj/Debug/WebApi.Tests.csprojAssemblyReference.cache Code/WebApi/BusinessLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll Code/WebApi/BusinessLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml Code/WebApi/DatabaseLayer/bin/Debug/Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.dll Code/WebApi/DatabaseLayer/bin/Debug/Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.xml Code/WebApi/DatabaseLayer/bin/Debug/Microsoft.VisualStudio.TestPlatform.TestFramework.dll Code/WebApi/DatabaseLayer/bin/Debug/Microsoft.VisualStudio.TestPlatform.TestFramework.xml Code/WebApi/DatabaseLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll Code/WebApi/DatabaseLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml Code/WebApi/ModelLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll Code/WebApi/ModelLayer/bin/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.xml Code/WebApi/packages/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.3.6.0/ Code/WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.CopyComplete Code/WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.CoreCompileInputs.cache Code/WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.csproj.FileListAbsolute.txt Code/WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.dll Code/WebApplication/WebApplication.Tests/obj/Debug/WebApplication.Tests.pdb Code/WebApplication/packages/Microsoft.CodeDom.Providers.DotNetCompilerPlatform.3.6.0/

--- Code/WebApi/UnitTestWebApiSolution/DataAccessLayer/TestDbAssignment.cs
+++ Code/WebApi/UnitTestWebApiSolution/DataAccessLayer/TestDbAssignment.cs
@@ -3,29 +3,50 @@ using DatabaseLayer.RepositoryLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModelLayer;
 using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
 
 namespace UnitTestWebApiSolution
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly IDbConnection _db;
+
+        public UnitTest1()
+        {
+            _db = new SqlConnection("Data Source = hildur.ucn.dk; Initial Catalog = dmaj0919_1081479; User ID = dmaj0919_1081479; Password=Password1!;");
+        }
+
         [TestMethod]
         public void TestCreateAssignment()
         {
             DbAssignmentIF dba = new DbAssignment();
             Assignment assignment = new Assignment();
-            DbUserIF dbu = new DbUser();
 
             assignment.Description = "Assignment description";
             assignment.Price = 10;
             assignment.Deadline = DateTime.Now;
             assignment.Anonymous = true;
 
-            User user = new User("fdsfds", "fdsfds", "fdsfds", "fdsfds", "fdsfds", "fdsfds");
-
             dba.CreateAssignment(assignment);
-            //dbu.InsertUser(user);
-            Assert.IsTrue(dba.GetAllAssignments().Count == 1);
+
+            Assert.IsTrue(dba.GetAllAssignments().Count > 0);
         }
+
+
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            DbAssignmentIF dba = new DbAssignment();
+            List<Assignment> kokot = dba.GetAllAssignments();
+
+            Assert.AreEqual(kokot.GetType(), typeof(List<Assignment>));
+        } 
     }
 }
