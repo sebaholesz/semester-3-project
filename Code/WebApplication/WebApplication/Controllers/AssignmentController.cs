ca340f384d33b926ca4f85e4437f1d09a4d5f70b
sebaholesz <sebastianholesz@seznam.cz>
2020-11-12 11:16:24 +0100

    Create assignment implementation - first version > the view, controller and model are implemented > the form is working and it is successfuly sending requests to the api > it also shows message based on the response status > some of the attributes are missing both in the view and in the model/api (e.g. title, academic level etc)

---
+++ Code/WebApplication/WebApplication/Controllers/AssignmentController.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class AssignmentController : Controller
+    {
+
+        [Route("assignment/create-assignment")]
+        [HttpGet]
+        public ActionResult CreateAssignment()
+        {
+            return View();
+        }
+
+        [Route("assignment/create-assignment")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAssignmentAsync(FormCollection collection)
+            {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var payload = new Dictionary<string, string>
+                    {
+                        {"Description", collection["assignmentDescription"]},
+                        {"Price", collection["assignmentCredits"]},
+                        {"Deadline", collection["assignmentDeadline"]},
+                        {"Anonymous", collection["assignmentIsAnonymous"]}
+                        //missing some of the values both from model and from the view
+                    };
+
+                    string strPayload = JsonConvert.SerializeObject(payload);
+                    HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
+
+                    string u = "https://localhost:44383/api/assignment";
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage message = client.PostAsync(u, c).Result;
+                        var responseContent = await message.Content.ReadAsStringAsync();
+                        var responseContentTrimmed = responseContent.Trim('\"');
+                        ViewBag.Message = responseContentTrimmed;
+                        ViewBag.StatusCode = message.StatusCode;
+                    }
+                }
+                return View("CreateAssignment");
+            }
+            catch
+            {
+                return View("CreateAssignment");
+            }
+        }
+    }
+}
---
+++ Code/WebApplication/WebApplication/Models/AssignmentModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class AssignmentModels
+    {
+        public int AssignmentId { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        // public List<Solution> Queue { get; set; }
+        public DateTime Deadline { get; set; }
+        public Boolean Anonymous { get; set; }
+    }
+}
