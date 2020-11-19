51e4e24a8957d6b6ea58f718be0b0512d2e38863
sebaholesz <sebastianholesz@seznam.cz>
2020-11-19 16:02:07 +0100

    Added server-side Input validation for assignment > + unit test coverage

---
+++ Code/WebApi/BusinessLayer/Validation/AssignmentInputValidation.cs
@@ -0,0 +1,90 @@
+﻿using DatabaseLayer.DataAccessLayer;
+using DatabaseLayer.RepositoryLayer;
+using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validation
+{
+    public class AssignmentInputValidation
+    {
+        private DbAssignmentIF dbA;
+
+        public AssignmentInputValidation()
+        {
+            dbA = new DbAssignment();
+        }
+
+        private const int _titleMinLength = 5;
+        private const int _titleMaxLength = 30;
+
+        private const int _descriptionMinLength = 1;
+        private const int _descriptionMaxLength = 500;
+
+        private const int _priceMinValue = 0;
+        private const int _priceMaxValue = 10000;
+
+        public bool CheckInput(Assignment assignment) 
+        {
+            //default return is true
+            bool value = true;
+            foreach (var item in assignment.GetType().GetProperties())
+            {
+                switch (item.Name)
+                {
+                    case "Title":
+                        value = assignment.Title.Length > _titleMinLength && assignment.Title.Length < _titleMaxLength;
+                        break;
+
+                    case "Description":
+                        value = assignment.Description.Length > _descriptionMinLength && assignment.Description.Length < _descriptionMaxLength;
+                        break;
+
+                    case "Price":
+                        value = assignment.Price > _priceMinValue && assignment.Price < _priceMaxValue;
+                        break;
+
+                    case "Deadline":
+                        value = DateTime.Compare(assignment.Deadline, DateTime.Now) > 0;
+                        break;
+
+                    case "AcademicLevel":
+                        var academicLevels = dbA.GetAllAcademicLevels();
+
+                        foreach (var al in academicLevels)
+                        {
+                            if (assignment.AcademicLevel.Equals(al))
+                            {
+                                value = true;
+                                break;
+                            }
+                            value = false;
+                        }
+                        break;
+
+                    case "Subject":
+                        var subjects = dbA.GetAllSubjects();
+
+                        foreach (var s in subjects)
+                        {
+                            if (assignment.Subject.Equals(s))
+                            {
+                                value = true;
+                                break;
+                            }
+                            value = false;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                if (!value) break;
+            }
+            return value;
+        }
+    }
+}
---
+++ Code/WebApi/UnitTestWebApiSolution/Validation/TestAssignmentInputValidation.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using BusinessLayer.Validation;
+using DatabaseLayer.RepositoryLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer;
+
+namespace UnitTestWebApiSolution
+{
+    [TestClass]
+    public class TestAssignmentInputValidation
+    {
+        private AssignmentInputValidation assignmentInputValidation;
+        private DateTime deadline = DateTime.Now;
+
+        public TestAssignmentInputValidation()
+        {
+            assignmentInputValidation = new AssignmentInputValidation();
+        }
+
+        [TestMethod]
+        public void TestAssignmentInputCorrect()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = 10;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsTrue(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentTitleInputIncorrectTooShort()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "blob";
+            assignment.Description = "Assignment description";
+            assignment.Price = 10;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentTitleInputIncorrectTooLong()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "This title is just too long to pass the test, you will see";
+            assignment.Description = "Assignment description";
+            assignment.Price = 10;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentDescriptionInputIncorrectTooShort()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "just some title";
+            assignment.Description = "";
+            assignment.Price = 10;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentDescriptionInputIncorrectTooLong()
+        {
+            Assignment assignment = new Assignment();
+
+            #region tooLongDescription
+            string tooLongDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis eros scelerisque, posuere orci eu, gravida libero. Aliquam placerat nulla a iaculis suscipit. Mauris accumsan pharetra nisi, posuere semper massa venenatis eu. Sed hendrerit auctor diam, vel vestibulum enim mollis vitae. Aenean luctus, dolor ut suscipit venenatis, sem augue ultricies turpis, non tempus lorem lorem sed felis. Suspendisse interdum tincidunt arcu ut lacinia. Maecenas nisi nisi, ultrices non cursus non, fringilla efficitur orci. Fusce lobortis nisl ultrices dui bibendum, quis ultrices nisl rhoncus. Nullam et "
+            #endregion
+
+            assignment.Title = "just some title";
+            assignment.Description = tooLongDescription;
+            assignment.Price = 10;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentPriceInputIncorrectTooLow()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = -1;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentPriceInputIncorrectToHigh()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = 500000000;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentDeadlineInputIncorrectEarlierDateTime()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = 500000000;
+            assignment.Deadline = deadline.AddDays(-5);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentAcademicLevelInputIncorrect()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = 500000000;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "blob";
+            assignment.Subject = "History";
+
+            Assert.IsFalse(assignmentInputValidation.CheckInput(assignment));
+        }
+
+        [TestMethod]
+        public void TestAssignmentSubjectInputIncorrect()
+        {
+            Assignment assignment = new Assignment();
+
+            assignment.Title = "Just some title";
+            assignment.Description = "Assignment description";
+            assignment.Price = 500000000;
+            assignment.Deadline = deadline.AddDays(1);
+            assignment.AcademicLevel = "High School First Grade";
+            assignment.Subject = "blob";
+        }
+    }
+}
