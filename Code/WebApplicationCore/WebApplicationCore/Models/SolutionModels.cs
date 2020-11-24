fb2a65c74c6031feb018e773f83190f7076efed7
sebaholesz <sebastianholesz@seznam.cz>
2020-11-24 18:30:45 +0100

    >Repaired layout for creating solution

--- Code/WebApplicationCore/WebApplicationCore/Models/SolutionModels.cs
+++ Code/WebApplicationCore/WebApplicationCore/Models/SolutionModels.cs
@@ -11,9 +11,10 @@ namespace WebApplicationCore.Models
     public class SolutionModels : Controller
     {
         public int SolutionId { get; set; }
-        [Required]
         public int AssignmentId { get; set; }
         public int UserId { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string Description { get; set; }
         public DateTime Timestamp { get; set; }
         public decimal SolutionRating { get; set; }
