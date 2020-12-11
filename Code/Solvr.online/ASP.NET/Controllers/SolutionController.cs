52e48026f360cc40bd860468538d00519368626d
sebaholesz <sebastianholesz@seznam.cz>
2020-12-11 18:40:31 +0100

    > Packages imported

--- Code/Solvr.online/ASP.NET/Controllers/SolutionController.cs
+++ Code/Solvr.online/ASP.NET/Controllers/SolutionController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
--- Code/Solvr.online/ASP.NET/Controllers/UserController.cs
+++ Code/Solvr.online/ASP.NET/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
